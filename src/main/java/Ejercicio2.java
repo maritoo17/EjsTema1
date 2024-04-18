57fc501394c56619da1d4c4e4fad09fca25bb35f
maritoo17 <msancgim@myuax.com>
2024-04-18 20:02:39 +0200

    2

---
+++ src/main/java/Ejercicio2.java
@@ -0,0 +1,11 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int maxInt = Integer.MAX_VALUE;
+        int maxShort = Short.MAX_VALUE;
+        char maxChar = Character.MAX_VALUE;
+
+        System.out.println ("El mayor número entero que se puede representar con un int es: " + maxInt);
+        System.out.println ("El mayor número entero que se puede representar con un short es: " + maxShort);
+        System.out.println ("El mayor número entero que se puede representar con un char es: " + (int)maxChar);
+    }
+}
